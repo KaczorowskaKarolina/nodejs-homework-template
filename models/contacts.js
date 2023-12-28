b049532b6d803626d091c82727f28bae9b0ca7c1
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-12-28 17:48:10 +0100

    modification in models/contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,80 @@
-// const fs = require('fs/promises')
+// models/contacts.js
 
-const listContacts = async () => {}
+const fs = require('fs/promises');
+const { v4: uuidv4 } = require('uuid');
+const Joi = require('joi');
 
-const getContactById = async (contactId) => {}
+const contactsFilePath = 'models/contacts.json';
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contactsData = await fs.readFile(contactsFilePath, 'utf-8');
+  return JSON.parse(contactsData);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  return contacts.find((contact) => contact.id === contactId);
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+  await fs.writeFile(contactsFilePath, JSON.stringify(updatedContacts, null, 2));
+};
+
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+
+  // Validation using Joi
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+  });
+
+  const validationResult = schema.validate({ name, email, phone });
+
+  if (validationResult.error) {
+    throw new Error(validationResult.error.details[0].message);
+  }
+
+  const contacts = await listContacts();
+  const newContact = { id: uuidv4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+
+  // Validation using Joi
+  const schema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string().email(),
+    phone: Joi.string(),
+  });
+
+  const validationResult = schema.validate({ name, email, phone });
+
+  if (validationResult.error) {
+    throw new Error(validationResult.error.details[0].message);
+  }
+
+  const contacts = await listContacts();
+  const updatedContacts = contacts.map((contact) =>
+    contact.id === contactId ? { ...contact, ...body } : contact
+  );
+
+  await fs.writeFile(contactsFilePath, JSON.stringify(updatedContacts, null, 2));
+
+  const updatedContact = updatedContacts.find((contact) => contact.id === contactId);
+  if (!updatedContact) {
+    throw new Error('Contact not found');
+  }
+
+  return updatedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +82,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
