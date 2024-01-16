fd156a5fbe56af938d6e1bd9e3fa0a050344385b
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2024-01-16 02:11:12 +0100

    add mongoose

--- models/contacts.mjs
+++ models/contacts.mjs
@@ -1,79 +1,70 @@
 // models/contacts.mjs
 
-import { readFile, writeFile } from 'fs/promises';
-import uuid from 'uuid';
-const { v4: uuidv4 } = uuid;
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
-import Joi from 'joi';
+mongoose.connect('mongodb+srv://kaczorowskakarolina:5ygs2sbnXTVXdIwb@cluster0.wesoj4z.mongodb.net/', {
+   useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const contactsFilePath = 'models/contacts.json';
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
 
 const listContacts = async () => {
-  const contactsData = await readFile(contactsFilePath, 'utf-8');
-  return JSON.parse(contactsData);
+  try {
+    return await Contact.find();
+  } catch (error) {
+    throw new Error(`Error listing contacts: ${error.message}`);
+  }
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  return contacts.find((contact) => contact.id === contactId);
+  try {
+    return await Contact.findById(contactId);
+  } catch (error) {
+    throw new Error(`Error getting contact by ID: ${error.message}`);
+  }
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
-  await writeFile(contactsFilePath, JSON.stringify(updatedContacts, null, 2));
+  try {
+    await Contact.findByIdAndRemove(contactId);
+  } catch (error) {
+    throw new Error(`Error removing contact: ${error.message}`);
+  }
 };
 
 const addContact = async (body) => {
-  const { name, email, phone } = body;
-
-  const schema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().required(),
-  });
-
-  const validationResult = schema.validate({ name, email, phone });
-
-  if (validationResult.error) {
-    throw new Error(validationResult.error.details[0].message);
+  try {
+    return await Contact.create(body);
+  } catch (error) {
+    throw new Error(`Error adding contact: ${error.message}`);
   }
-
-  const contacts = await listContacts();
-  const newContact = { id: uuidv4(), name, email, phone };
-  contacts.push(newContact);
-  await writeFile(contactsFilePath, JSON.stringify(contacts, null, 2));
-  return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  const { name, email, phone } = body;
-
-  const schema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string().email(),
-    phone: Joi.string(),
-  });
-
-  const validationResult = schema.validate({ name, email, phone });
-
-  if (validationResult.error) {
-    throw new Error(validationResult.error.details[0].message);
+  try {
+    return await Contact.findByIdAndUpdate(contactId, body, { new: true });
+  } catch (error) {
+    throw new Error(`Error updating contact: ${error.message}`);
   }
-
-  const contacts = await listContacts();
-  const updatedContacts = contacts.map((contact) =>
-    contact.id === contactId ? { ...contact, ...body } : contact
-  );
-
-  await writeFile(contactsFilePath, JSON.stringify(updatedContacts, null, 2));
-
-  const updatedContact = updatedContacts.find((contact) => contact.id === contactId);
-  if (!updatedContact) {
-    throw new Error('Contact not found');
-  }
-
-  return updatedContact;
 };
 
 export {
@@ -83,3 +74,4 @@ export {
   addContact,
   updateContact,
 };
+
