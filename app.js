8f27b2b7ed8508e7f6c46db2abe606ac804c31aa
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2024-01-18 13:05:28 +0100

    app.js with added support for express.urlencoded

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/contacts', contactsRouter);
 
 app.listen(PORT, () => {
